dcffcd639924eda000b61f60ff8e796584eb742e
maeriii <everlynesinga@gmail.com>
2025-05-02 11:55:37 +0300

    add

--- app/src/main/java/com/mike/booknest/data/UserDao.kt
+++ app/src/main/java/com/mike/booknest/data/UserDao.kt
@@ -5,7 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.nyokabi.harakamall.model.User
+import com.mike.booknest.model.User
 
 
 @Dao
